95275855503afebdd5a0464cac2ce12791bc6a02
anhtu6 <anhtu632@gmail.com>
2020-02-23 16:31:06 +0700

    do you love me

---
+++ js/app.js
@@ -0,0 +1,5 @@
+function changePosition() {
+    document.getElementById('buttonRight').style.marginLeft = parseInt(Math.random()*50)+'px';
+    document.getElementById('buttonRight').style.marginTop =parseInt(Math.random()*50)+'px';
+
+}
